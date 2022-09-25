7f4b8517bb174eb087c79376d27d578228362325
calango <mozartnp@gmail.com>
2022-09-25 03:13:34 -0300

    Some adjustment to dashboard

--- backend/apps/core/static/extra/sidebar/sidebar.js
+++ backend/apps/core/static/extra/sidebar/sidebar.js
@@ -1,15 +1,15 @@
 var sidebar = document.getElementById("sideBar")
 function manipularNav() {
-    if (sidebar.classList.contains("fechado")) {
-        sidebar.classList.add("aberto");
-        sidebar.classList.remove("fechado");
-        document.getElementById("botaoSideBar").style.marginLeft = "125px";
+    if (sidebar.classList.contains("close")) {
+        sidebar.classList.add("open");
+        sidebar.classList.remove("close");
+        document.getElementById("buttonSideBar").style.marginLeft = "125px";
         document.getElementById("sideBar").style.width = "180px";
         document.getElementById("conteudo").style.marginLeft = "180px";
-    } else if (sidebar.classList.contains("aberto")) {
-        sidebar.classList.add("fechado");
-        sidebar.classList.remove("aberto");
-        document.getElementById("botaoSideBar").style.marginLeft = "0";
+    } else if (sidebar.classList.contains("open")) {
+        sidebar.classList.add("close");
+        sidebar.classList.remove("open");
+        document.getElementById("buttonSideBar").style.marginLeft = "0";
         document.getElementById("sideBar").style.width = "0";
         document.getElementById("conteudo").style.marginLeft = "10px";
     }
